e32f4f98b97a3b93618bcc632b9a53990d6fe02d
Kaeden Bryer <kaedenbryer@gmail.com>
2026-06-19 22:44:18 -0400

    make more dynamic

--- randomizer.py
+++ randomizer.py
@@ -20,27 +20,43 @@ pairs = [
     "Jamie & Harold",
     "Aude & Ashley",
     "Kristin & Connor",
-    "Emma & TBD",
+    "Emma & AJ",
     "Duyen & Ian",
     "Jaina & LJ",
     "Benny & Kaylee",
     "Mitch & Grace",
     "Nathan & Teresa",
     "Sarah & Jaden",
-    "Ghost",
-    "Ghost",
-    "Ghost",
-    "Ghost",
 ]
 
-def make_pool(pairs, pool_num):
-    print("Pool " + str(pool_num) + ":")
-    for i in range(1,7):
-        pair_num = random.randint(0, len(pairs) - 1)
-        print(pairs[pair_num])
-        pairs.pop(pair_num)
-    print("")
+def make_pools(pairs, pool_count, ghosts):
+
+    pools = [[] for _ in range(pool_count)]
+
+    i = 0
+    while pairs:
+        if ghosts > 0:
+            pools[i].append("Ghost")
+            ghosts -= 1
+        else:
+            rand_num = random.randint(0, len(pairs) - 1)
+            pools[i].append(pairs[rand_num])
+            pairs.pop(rand_num)
+
+        i = (i+1) % pool_count
+
+    return pools
 
 if __name__ == "__main__":
-    for i in range(1,6):
-        make_pool(pairs, i)
+    pool_count = 5
+    pool_size = 6
+
+    ghosts = pool_count * pool_size - len(pairs)
+    
+    pools = make_pools(pairs, pool_count, ghosts)
+
+    for i, pool in enumerate(pools):
+        print("Pool " + str(i + 1) + ":")
+        for pair in pool:
+            print(pair)
+        print()
