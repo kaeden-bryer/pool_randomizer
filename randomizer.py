f61818877e842592a6c2d61710f29700f1f7de05
Kaeden Bryer <kaedenbryer@gmail.com>
2026-06-19 20:04:28 -0400

    can't count oops

--- randomizer.py
+++ randomizer.py
@@ -35,12 +35,12 @@ pairs = [
 
 def make_pool(pairs, pool_num):
     print("Pool " + str(pool_num) + ":")
-    for i in range(1,6):
+    for i in range(1,7):
         pair_num = random.randint(0, len(pairs) - 1)
         print(pairs[pair_num])
         pairs.pop(pair_num)
     print("")
 
 if __name__ == "__main__":
-    for i in range(1,5):
+    for i in range(1,6):
         make_pool(pairs, i)
